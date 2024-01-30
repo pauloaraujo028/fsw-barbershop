38abbbb984116ae705daa8472b27f763d3cccecd
Paulo Araújo <pauloaraujo.phz@gail.com>
2024-01-30 14:03:12 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header />
-    </div>
+
+      <section className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Paulo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </section>
+    </main>
   );
 }
