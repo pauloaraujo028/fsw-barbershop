4b04344467f6535f3b491742cad507747e34cf6f
Paulo Araújo <pauloaraujo.phz@gail.com>
2024-02-05 08:35:52 -0300

    feat: ensure first available time to book a service is the current time plus 30 minutes

--- app/barbershops/[id]/_helpers/hours.ts
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -1,15 +1,18 @@
-import { addMinutes, format, setHours, setMinutes } from "date-fns";
+import { addMinutes, format, isAfter, setHours, setMinutes } from "date-fns";
 
 export function generateDayTimeList(date: Date): string[] {
-  const startTime = setMinutes(setHours(date, 9), 0); // Set start time to 09:00
+  const currentDateTime = addMinutes(new Date(), 30);
+  const startTime = setMinutes(setHours(date, 7), 0); // Set start time to 09:00
   const endTime = setMinutes(setHours(date, 21), 0); // Set end time to 21:00
-  const interval = 45; // Interval in minutes
+  const interval = 30; // Interval in minutes
   const timeList: string[] = [];
 
   let currentTime = startTime;
 
   while (currentTime <= endTime) {
-    timeList.push(format(currentTime, "HH:mm"));
+    if (isAfter(currentTime, currentDateTime)) {
+      timeList.push(format(currentTime, "HH:mm"));
+    }
     currentTime = addMinutes(currentTime, interval);
   }
 
